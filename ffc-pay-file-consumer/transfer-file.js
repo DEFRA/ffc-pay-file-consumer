2b30afcecc82c54faebafdf98fcbccf52e890a6c
John Watson <johnwatson484@hotmail.com>
2022-03-15 21:05:20 +0000

    Remove await

--- ffc-pay-file-consumer/transfer-file.js
+++ ffc-pay-file-consumer/transfer-file.js
@@ -3,7 +3,7 @@ const getFile = require('./get-file')
 const storage = require('./storage')
 
 const transferFile = async (context, request) => {
-  await storage.connect(blobConnectionString, shareConnectionString, shareName)
+  storage.connect(blobConnectionString, shareConnectionString, shareName)
   const { file, content } = await getFile(context, request.filepath)
   await storage.writeFile(request.filename, content)
   await storage.deleteFile(file)
